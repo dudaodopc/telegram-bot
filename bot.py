cf74c5b50b69ae19268c783c5a93b04252a09e6f
dudaodopc <eduardatrader@hotmail.com>
2026-01-18 12:50:08 -0300

    add telegram bot

---
+++ bot.py
@@ -0,0 +1,19 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "🤖 Bot online com sucesso!\n\n"
+        "Agora você já pode me personalizar 😉"
+    )
+
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+if name == "__main__":
+    main()
